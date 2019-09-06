1eecf12de66bbd2c5f875b2a82d7b38b41f60776
Clay Dunston <claydunston@gmail.com>
2019-09-06 10:37:49 -0500

    Rename receiver variable to `ts` in Templates methods

--- tmpl/templates.go
+++ tmpl/templates.go
@@ -15,12 +15,12 @@ type Templates struct {
 }
 
 // Add a new template.
-func (tmpls *Templates) Add(newTmpl Template) error {
-	if tmpls.NameExists(newTmpl.Name) {
+func (ts *Templates) Add(newTmpl Template) error {
+	if ts.NameExists(newTmpl.Name) {
 		return fmt.Errorf("A template named %q already exists", newTmpl.Name)
 	}
-	tmpls.T = append(tmpls.T, newTmpl)
-	err := tmpls.Save()
+	ts.T = append(ts.T, newTmpl)
+	err := ts.Save()
 	if err != nil {
 		return err
 	}
@@ -28,20 +28,20 @@ func (tmpls *Templates) Add(newTmpl Template) error {
 }
 
 // Remove an existing template, itendified by its name.
-func (tmpls *Templates) Remove(name string) error {
-	templates := make([]Template, len(tmpls.T)-1)
-	if !tmpls.NameExists(name) {
+func (ts *Templates) Remove(name string) error {
+	templates := make([]Template, len(ts.T)-1)
+	if !ts.NameExists(name) {
 		return fmt.Errorf("No template named %q found", name)
 	}
 	i := 0
-	for _, tmpl := range tmpls.T {
+	for _, tmpl := range ts.T {
 		if tmpl.Name != name {
 			templates[i] = tmpl
 			i++
 		}
 	}
-	tmpls.T = templates
-	err := tmpls.Save()
+	ts.T = templates
+	err := ts.Save()
 	if err != nil {
 		return err
 	}
@@ -49,8 +49,8 @@ func (tmpls *Templates) Remove(name string) error {
 }
 
 // GetByName returns a template with a given name and an error if no template is found.
-func (tmpls Templates) GetByName(name string) (Template, error) {
-	for _, tmpl := range tmpls.T {
+func (ts Templates) GetByName(name string) (Template, error) {
+	for _, tmpl := range ts.T {
 		if tmpl.Name == name {
 			return tmpl, nil
 		}
@@ -60,8 +60,8 @@ func (tmpls Templates) GetByName(name string) (Template, error) {
 }
 
 // NameExists checks to see if a given name is already being used for a template.
-func (tmpls *Templates) NameExists(name string) bool {
-	for _, tmpl := range tmpls.T {
+func (ts *Templates) NameExists(name string) bool {
+	for _, tmpl := range ts.T {
 		if tmpl.Name == name {
 			return true
 		}
@@ -71,11 +71,11 @@ func (tmpls *Templates) NameExists(name string) bool {
 }
 
 // Save templates to a user's templates.json file.
-func (tmpls Templates) Save() error {
+func (ts Templates) Save() error {
 	dataDir := viper.GetString("templatesdir")
 	dataFile := filepath.Join(dataDir, "templates.json")
 
-	bytes, err := json.MarshalIndent(tmpls, "", "  ")
+	bytes, err := json.MarshalIndent(ts, "", "  ")
 	if err != nil {
 		return err
 	}
@@ -90,7 +90,7 @@ func (tmpls Templates) Save() error {
 
 // GetTemplates returns the user's templates.
 func GetTemplates() (Templates, error) {
-	var tmpls Templates
+	var ts Templates
 	tmplDir := viper.GetString("templatesdir")
 	tmplFile := filepath.Join(tmplDir, "templates.json")
 
@@ -98,21 +98,21 @@ func GetTemplates() (Templates, error) {
 	if err != nil {
 		// Return nil if tmplFile doesn't exist yet.
 		// We'll make on Templates.Save().
-		return tmpls, nil
+		return ts, nil
 	}
 
-	err = json.Unmarshal(bytes, &tmpls)
+	err = json.Unmarshal(bytes, &ts)
 	if err != nil {
-		return tmpls, err
+		return ts, err
 	}
 
-	return tmpls, nil
+	return ts, nil
 }
 
 // Names returns the names of all templates.
-func (tmpls Templates) Names() []string {
-	names := make([]string, len(tmpls.T))
-	for i, tmpl := range tmpls.T {
+func (ts Templates) Names() []string {
+	names := make([]string, len(ts.T))
+	for i, tmpl := range ts.T {
 		names[i] = tmpl.Name
 	}
 	return names
