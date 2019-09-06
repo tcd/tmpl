dee9d03f5f763136bac7286fe3a15029628d2777
Clay Dunston <claydunston@gmail.com>
2019-09-06 10:31:04 -0500

    Move setup & teardown into one file

--- tmpl/updown.go
+++ tmpl/updown.go
@@ -7,10 +7,49 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/AlecAivazis/survey/v2"
 	homedir "github.com/mitchellh/go-homedir"
 )
 
-// Setup tmpl.
+// Teardown removes all files & folders used by tmpl.
+// Should be called before uninstalling.
+func Teardown() {
+	doIt := false
+	prompt := &survey.Confirm{
+		Message: "Are you sure you want to remove all files & folders related to tmpl?",
+	}
+	survey.AskOne(prompt, &doIt)
+	if !doIt {
+		return
+	}
+
+	log.Println("Sad to see you go :(")
+	dataDir := DefaultDataDir()
+
+	if doesExist(dataDir) {
+		err := os.RemoveAll(dataDir)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("Data directory removed")
+	}
+
+	home, err := homedir.Dir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	cfgFile := filepath.Join(home, ".tmpl.json")
+	if doesExist(cfgFile) {
+		err := os.Remove(cfgFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("Config file removed")
+	}
+}
+
+// Setup creates all files & folders used by tmpl.
+// Should be called directly after installing.
 func Setup() {
 	dataDir := DefaultDataDir()
 
