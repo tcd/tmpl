98ee61ca8291f1c2da42fc2d29d9107781e710a8
Clay Dunston <claydunston@gmail.com>
2019-09-06 19:14:09 -0500

    Add remaining fields to edit subcommand

--- cmd/edit.go
+++ cmd/edit.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"strings"
 
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/gookit/color"
 	"github.com/spf13/cobra"
 	"github.com/tcd/extedit"
 	"github.com/tcd/tmpl/tmpl"
@@ -23,25 +25,10 @@ var editCmd = &cobra.Command{
 		}
 	},
 	Run: func(cmd *cobra.Command, args []string) {
-		ts, err := tmpl.GetTemplates()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		name := tmpl.PickTemplate("Please choose a template to edit:")
-		t, _ := ts.GetByName(name)
-		input := strings.NewReader(t.Content)
-		diff, err := extedit.Invoke(input)
-		if err != nil {
-			log.Fatal(err)
-		}
-		t.Content = diff.Content()
-		err = ts.Update(t)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println("Template updated")
-		os.Exit(0)
+		IfBoolFlag(cmd, "content", editContent)
+		IfBoolFlag(cmd, "name", editName)
+		IfBoolFlag(cmd, "file", editFileName)
+		IfBoolFlag(cmd, "data", editData)
 	},
 }
 
@@ -49,7 +36,143 @@ func init() {
 	rootCmd.AddCommand(editCmd)
 
 	editCmd.Flags().Bool("content", false, "Edit a template's contents")
-	editCmd.Flags().Bool("data", false, "Edit a template's data")
-	editCmd.Flags().Bool("name", false, "Edit a template's name")
 	editCmd.Flags().Bool("file", false, "Edit a template's filename")
+	editCmd.Flags().Bool("name", false, "Edit a template's name")
+	editCmd.Flags().Bool("data", false, "Edit a template's data")
+}
+
+func editContent() {
+	ts, err := tmpl.GetTemplates()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	name := tmpl.PickTemplate("Please choose a template to edit:")
+	t, _ := ts.GetByName(name)
+
+	input := strings.NewReader(t.Content)
+	diff, err := extedit.Invoke(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	t.Content = diff.Content()
+	err = ts.Update(t)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Template %s updated\n", color.FgBlue.Render(t.Name))
+}
+
+func editName() {
+	ts, err := tmpl.GetTemplates()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	name := tmpl.PickTemplate("Please choose a template to edit:")
+	t, _ := ts.GetByName(name)
+
+	msg := "Please choose a new name:"
+	for {
+		var newName string
+		prompt := &survey.Input{Message: msg}
+		survey.AskOne(prompt, &newName)
+		if !ts.NameExists(newName) {
+			t.Name = name
+			break
+		}
+		msg = "A template that name already exists. Please choose another:"
+	}
+
+	err = ts.Update(t)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Template %s updated\n", color.FgBlue.Render(t.Name))
+}
+
+func editFileName() {
+	ts, err := tmpl.GetTemplates()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	name := tmpl.PickTemplate("Please choose a template to edit:")
+	t, _ := ts.GetByName(name)
+
+	var newName string
+	prompt := &survey.Input{Message: "Please choose a new filename:"}
+	survey.AskOne(prompt, &newName)
+
+	t.FileName = newName
+	err = ts.Update(t)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Template %s updated\n", color.FgBlue.Render(t.Name))
+}
+
+func editData() {
+	ts, err := tmpl.GetTemplates()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	name := tmpl.PickTemplate("Please choose a template to edit:")
+	t, _ := ts.GetByName(name)
+
+	var action string
+	prompt := &survey.Select{
+		Message: "What would you like to do?:",
+		Options: []string{"add data", "remove data"},
+	}
+	survey.AskOne(prompt, &action)
+
+	var newData []string
+	switch action {
+	case "add data":
+		newData = addData(t.Data)
+	case "remove data":
+		newData = removeData(t.Data)
+	default:
+		os.Exit(1)
+	}
+	t.Data = newData
+	err = ts.Update(t)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Template %s updated\n", color.FgBlue.Render(t.Name))
+}
+
+func addData(data []string) []string {
+	for {
+		var name string
+		prompt1 := &survey.Input{Message: "Enter a name for a new variable"}
+		survey.AskOne(prompt1, &name)
+		if contains(data, name) {
+			log.Println("A variable with this name already exists")
+		} else {
+			data = append(data, name)
+		}
+
+		var more bool
+		prompt2 := &survey.Confirm{Message: "Add another?"}
+		survey.AskOne(prompt2, &more)
+		if !more {
+			break
+		}
+	}
+	return data
+}
+
+func removeData(data []string) []string {
+	toRemove := []string{}
+	prompt := &survey.MultiSelect{
+		Message: "What days do you prefer:",
+		Options: data,
+	}
+	survey.AskOne(prompt, &toRemove)
+	return remove(data, toRemove...)
 }
--- cmd/lib.go
+++ cmd/lib.go
@@ -1,9 +1,25 @@
 package cmd
 
 import (
+	"log"
 	"os"
 	"strings"
+
+	"github.com/spf13/cobra"
 )
+
+// IfBoolFlag executes a function if a given bool flag is true.
+// `os.Exit(0)` is called if successFunc sucessfully runs.
+func IfBoolFlag(cmd *cobra.Command, flagName string, successFunc func()) {
+	boolFlag, err := cmd.Flags().GetBool(flagName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if boolFlag {
+		successFunc()
+		os.Exit(0)
+	}
+}
 
 // doesExist returns true if a file or folder already exists.
 func doesExist(path string) bool {
@@ -25,4 +41,37 @@ func titleString() string {
 	}
 
 	return strings.Join(lines, "\n")
+}
+
+// =============================================================================
+// String Slice Functions
+// =============================================================================
+
+func contains(slice []string, str string) bool {
+	for _, s := range slice {
+		if s == str {
+			return true
+		}
+	}
+	return false
+}
+
+// remove returns a new slice with all given strings removed.
+func remove(slice []string, toRemove ...string) []string {
+	newSlice := make([]string, len(slice)-len(toRemove))
+	i := 0
+	for _, s := range slice {
+		match := false
+		for _, str := range toRemove {
+			if str == s {
+				match = true
+				break
+			}
+		}
+		if !match {
+			newSlice[i] = s
+			i++
+		}
+	}
+	return newSlice
 }
